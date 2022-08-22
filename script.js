bf860cbb997c447c772ad5f37d3243592d8600da
thejesscanada <canada.jess98@gmail.com>
2022-08-22 16:56:04 -0500

    started to add js for start btn

---
+++ script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+const questionContainer
+startButton.addEventListener('click', startgame)
+
+function startgame() {
+    console.log('started')
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
