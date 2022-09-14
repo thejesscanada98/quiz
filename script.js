9a7f0b20f0715a4ac09cf09ef69fabd22112fa47
thejesscanada <canada.jess98@gmail.com>
2022-09-14 00:39:59 -0500

    added timer

--- script.js
+++ script.js
@@ -20,6 +20,23 @@ function startGame() {
   setNextQuestion()
 }
 
+var count = 15;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    alert("You're out of time!");
+  }
+}, 1000);
+
+var stopInterval = function() {
+  console.log('time is up!');
+  clearInterval(timer);
+}
+
 function setNextQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
